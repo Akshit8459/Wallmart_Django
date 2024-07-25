24665fea139b00b8ca29422695f257171650eb34
Akshit8459 <akshitpokhriyal@gmail.com>
2024-07-26 00:20:15 +0530

    forgot to migrate :>

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import TodoItem
+from .models import TodoItem, Supplier, DeliveryRoute, EmissionData
 # Register your models here.
 admin.site.register(TodoItem)
-
+admin.site.register(Supplier)
+admin.site.register(DeliveryRoute)
+admin.site.register(EmissionData)
 
