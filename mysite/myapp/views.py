118ca08e42e2628c9380c27a960206fa295593f6
igen-aru <111181527+igen-aru@users.noreply.github.com>
2024-08-09 22:37:17 +0530

    Update views.py

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,4 +1,3 @@
-# views.py
 from rest_framework import viewsets
 from .models import Product, CompetitorPrice, PriceHistory, DemandForecast
 from .serializers import ProductSerializer, CompetitorPriceSerializer, PriceHistorySerializer, DemandForecastSerializer
