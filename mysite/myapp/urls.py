be90cda7478680c538a5752972e28c612931514c
igen-aru <111181527+igen-aru@users.noreply.github.com>
2024-08-09 22:37:04 +0530

    Update urls.py

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,4 +1,3 @@
-# urls.py
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, CompetitorPriceViewSet, PriceHistoryViewSet, DemandForecastViewSet
