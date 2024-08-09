bb37209153936465c5efeedeb9a46dfe0fa8a087
igen-aru <111181527+igen-aru@users.noreply.github.com>
2024-08-09 22:41:51 +0530

    Update admin.py

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import TodoItem, Supplier, DeliveryRoute, EmissionData
-# Register your models here.
-admin.site.register(TodoItem)
-admin.site.register(Supplier)
-admin.site.register(DeliveryRoute)
-admin.site.register(EmissionData)
+from .models import Product, CompetitorPrice, PriceHistory, DemandForecast
 
+admin.site.register(Product)
+admin.site.register(CompetitorPrice)
+admin.site.register(PriceHistory)
+admin.site.register(DemandForecast)
